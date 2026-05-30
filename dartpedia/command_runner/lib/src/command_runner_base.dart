bc01d446941b319e918af7371fdf52a5663a5334
Natan do Carmo Silva <natan.c.silva@edu.senai.br>
2026-05-30 16:47:46 +0000

    Concertado erro no command_runner.dart

--- dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -1,6 +1,10 @@
+
+
+
 import 'dart:collection';
 import 'dart:io';
 import 'arguments.dart';
+
 
 class CommandRunner {
   final Map<String, Command> _commands = <String, Command>{};
@@ -40,11 +44,6 @@ class CommandRunner {
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library;
-
-export 'src/arguments.dart';
-export 'src/command_runner_base.dart';
-export 'src/help_command.dart';
 
 // TODO: Export any libraries intended for clients of this package.
 
