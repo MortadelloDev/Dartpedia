536c33cc4d8dba815d94cb10f6b39af23e652872
Micael Cacilha <micael.cacilha@edu.senai.br>
2026-06-10 20:53:53 +0000

    Exportado funções API para wikipedia.dart

--- dartpedia/wikipedia/bin/wikipedia.dart
+++ dartpedia/wikipedia/bin/wikipedia.dart
@@ -1,5 +1,9 @@
-import 'package:wikipedia/wikipedia.dart' as wikipedia;
+export 'src/api/get_article.dart';
+export 'src/api/search.dart';
+export 'src/api/summary.dart';
+export 'src/model/article.dart';
+export 'src/model/search_results.dart';
+export 'src/model/summary.dart';
+export 'src/model/title_set.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${wikipedia.calculate()}!');
-}
+
